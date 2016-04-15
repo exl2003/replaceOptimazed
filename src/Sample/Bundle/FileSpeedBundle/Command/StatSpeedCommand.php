e011bdfaf7e464cfad5dde79cefbdcfe7b53fcd7
Zarubin Alexey <exl2003@mail.ru>
2016-04-15 14:27:44 +0300

    small config params

--- src/Sample/Bundle/FileSpeedBundle/Command/StatSpeedCommand.php
+++ src/Sample/Bundle/FileSpeedBundle/Command/StatSpeedCommand.php
@@ -81,10 +81,56 @@ class StatSpeedCommand extends ContainerAwareCommand
          */
         foreach ($this->servers as $server) {
             if ( $server != $myHost ){
-                echo "$server\n";
+                //echo "$server\n";
+                $url = "http://{$server}/app_dev.php/file-speed-download";
+                $this->getUrl($url);
             }
             
         }
         
     }
+    
+    private function getUrl($url )
+    {
+
+            // Initialize cURL with given url
+            $ch = curl_init($url);
+
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_USERAGENT, 'zSpeedTest 1.0');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 60); //param
+            set_time_limit(65);//param
+
+            $execute = curl_exec($ch);
+            $info = curl_getinfo($ch);
+
+            // Time spent downloading, I think
+            $time = $info['total_time'] 
+                  - $info['namelookup_time'] 
+                  - $info['connect_time'] 
+                  - $info['pretransfer_time'] 
+                  - $info['starttransfer_time'] 
+                  - $info['redirect_time'];
+
+
+            // Echo friendly messages
+            //header('Content-Type: text/plain');
+            printf("Downloaded %d bytes in %0.4f seconds.\n", $info['size_download'], $time);
+            printf("Which is %0.4f mbps\n", $info['size_download'] * 8 / $time / 1024 / 1024);
+            printf("CURL said %0.4f mbps\n", $info['speed_download'] * 8 / 1024 / 1024);
+
+            echo "\n\ncurl_getinfo() said:\n", str_repeat('-', 31 + strlen($url)), "\n";
+            foreach ($info as $label => $value)
+            {
+                //if ('certinfo' != $label ){
+                    printf("%-30s %s\n", $label, empty($value) ? '' : $value );
+                /*} else  {
+                    var_dump( $label, $value ); exit;
+                }*/
+            }
+        
+    }
+    
 }
